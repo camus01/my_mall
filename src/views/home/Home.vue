5011e6aeced773e34408a94d48b3ad5a7187098d
camus01 <1186142547@qq.com>
2020-02-03 22:40:44 +0800

    add nav and tab

---
+++ src/views/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="home">
+    <nav-bar><div slot="center">知识服务平台</div></nav-bar>
+  </div>
+</template>
+<script>
+  import NavBar from 'components/common/navbar/NavBar'
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar
+    }
+  }
+</script>
+<style scoped>
+  .nav-bar {
+    background-color: var(--color, #858585);
+    font-weight: 700;
+    color: rgb(250, 250, 250);
+  }
+</style>
